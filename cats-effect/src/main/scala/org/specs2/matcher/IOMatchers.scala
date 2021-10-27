3f92711e5ce3497102be02c16fd2ed56f36f9e44
Arman Bilge <armanbilge@gmail.com>
2021-10-27 19:33:18 +0000

    Fix implementation of IOMatchers

--- cats-effect/src/main/scala/org/specs2/matcher/IOMatchers.scala
+++ cats-effect/src/main/scala/org/specs2/matcher/IOMatchers.scala
@@ -7,6 +7,7 @@ import cats.syntax.all.*
 import org.specs2.concurrent.ExecutionEnv
 import org.specs2.execute.Failure
 import org.specs2.execute.Result
+import org.specs2.execute.Error
 import org.specs2.execute.Success
 import org.specs2.text.Regexes.matchesSafely
 
@@ -21,46 +22,45 @@ trait IOMatchers extends ValueChecks:
 
   given ioRuntime: IORuntime = IORuntime.global
 
-  extension [A](ioa: IO[A])
-    private def unsafeFoldOutcome[B](canceled: =>B, errored: Throwable => B, completed: A => B): Future[B] =
-      ioa.start
-        .flatMap(_.join)
-        .flatMap { outcome =>
-          outcome.fold(IO(canceled), e => IO(errored(e)), _.map(completed))
-        }
-        .unsafeToFuture()
+  private def mkMatcher[A, B](transform: IO[A] => IO[B])(
+      canceled: =>Result,
+      errored: Throwable => Result,
+      completed: B => Result
+  ): FutureMatcher[IO[A]] =
+    new FutureMatcher[IO[A]]:
+      def apply[IOA <: IO[A]](ioa: IOA) =
+        transform(ioa).start
+          .flatMap(_.join)
+          .flatMap { outcome =>
+            outcome.fold(IO(canceled), e => IO(errored(e)), _.map(completed))
+          }
+          .unsafeToFuture()
 
   def beSuccess[A]: FutureMatcher[IO[A]] =
     beSuccess(ValueCheck.alwaysOk)
 
   def beSuccess[A](check: ValueCheck[A]): FutureMatcher[IO[A]] =
-    FutureMatcher(
-      _.unsafeFoldOutcome(
-        Failure("The IO was canceled"),
-        Error("The IO raised an error", _),
-        check.check
-      )
+    mkMatcher(identity[IO[A]])(
+      Failure("The IO was canceled"),
+      Error("The IO raised an error", _),
+      check.check
     )
 
   def beError[A]: FutureMatcher[IO[A]] =
     beError(ValueCheck.alwaysOk)
 
   def beError[A](check: ValueCheck[Throwable]): FutureMatcher[IO[A]] =
-    FutureMatcher(
-      _.>>(IO.trace).unsafeFoldOutcome(
-        Failure("The IO was canceled"),
-        check.check,
-        trace => Failure("The IO succeeded but it was expected to raise an error", trace = trace.toList)
-      )
+    mkMatcher(_ >> IO.trace)(
+      Failure("The IO was canceled"),
+      check.check,
+      trace => Failure("The IO succeeded but it was expected to raise an error", trace = trace.toList)
     )
 
   def beCanceled[A]: FutureMatcher[IO[A]] =
-    FutureMatcher(
-      _.>>(IO.trace).unsafeFoldOutcome(
-        Success("The IO was canceled"),
-        Error("The IO raised an error", _),
-        trace => Failure("The IO succeeded but it was expected to cancel", trace = trace.toList)
-      )
+    mkMatcher(_ >> IO.trace)(
+      Success("The IO was canceled"),
+      Error("The IO raised an error", _),
+      trace => Failure("The IO succeeded but it was expected to cancel", trace = trace.toList)
     )
 
   extension [T](action: IO[T])
