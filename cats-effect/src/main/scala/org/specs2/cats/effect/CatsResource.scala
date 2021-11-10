a6d4e110111d91821dca9a311f743417779f4ca4
Arman Bilge <armanbilge@gmail.com>
2021-11-10 21:10:19 +0000

    Install finalizer regardless of errors

--- cats-effect/src/main/scala/org/specs2/cats/effect/CatsResource.scala
+++ cats-effect/src/main/scala/org/specs2/cats/effect/CatsResource.scala
@@ -4,6 +4,7 @@ import cats.effect.Deferred
 import cats.effect.IO
 import cats.effect.Resource
 import cats.effect.unsafe.IORuntime
+import cats.syntax.all.*
 import org.specs2.specification.core.Execution
 import org.specs2.specification.Resource as SpecsResource
 
@@ -14,10 +15,11 @@ trait CatsResource[T](using ioRuntime: IORuntime = IORuntime.global) extends Spe
 
   def resource: Resource[IO, T]
 
-  protected def acquire: Future[T] = resource.allocated
+  protected def acquire: Future[T] = resource.attempt.allocated
     .flatMap { (t, close) =>
       finalizer.complete(close).as(t)
     }
+    .rethrow
     .unsafeToFuture()(ioRuntime)
 
   protected def release(resource: T): Execution = finalizer.get.flatten
