57584b0ddce580822bc609d3a761d0bea45ce01d
Arman Bilge <armanbilge@gmail.com>
2021-12-04 01:54:13 +0000

    Add config options

--- cats-effect/src/main/scala/org/specs2/cats/effect/CatsEffect.scala
+++ cats-effect/src/main/scala/org/specs2/cats/effect/CatsEffect.scala
@@ -1,3 +1,13 @@
 package org.specs2.cats.effect
 
-trait CatsEffect extends IOExecution, IOMatchers, SyncIOExecution, SyncIOMatchers, CatsResourceExecution
+import cats.effect.unsafe.IORuntime
+import org.specs2.concurrent.ExecutionEnv
+
+trait CatsEffect(using
+    executionEnv: ExecutionEnv = ExecutionEnv.fromGlobalExecutionContext,
+    ioRuntime: IORuntime = IORuntime.global
+) extends IOExecution,
+      IOMatchers,
+      SyncIOExecution,
+      SyncIOMatchers,
+      CatsResourceExecution
